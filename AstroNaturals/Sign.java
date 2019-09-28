4a42f4503aa8abbbdcf133718bbd3d010121d5c8
Empy12 <theairest@outlook.com>
2019-09-28 16:07:55 -0400

    Delete Sign.java

--- AstroNaturals/Sign.java
+++
@@ -1,7 +0,0 @@
-
-public interface Sign {
-	public String getSign(int month, int day); 
-	public String getFact(String sign);
-	public String getActivism(String sign); 
-	public String getStarName();
-}
