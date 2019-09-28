92a1b67e559e2755112240854fc4b1e51eced327
Empy12 <theairest@outlook.com>
2019-09-28 15:07:24 -0400

    Add files via upload

---
+++ AstroNaturals/Sign.java
@@ -0,0 +1,7 @@
+
+public interface Sign {
+	public String getSign(int month, int day); 
+	public String getFact(String sign);
+	public String getActivism(String sign); 
+	public String getStarName();
+}
